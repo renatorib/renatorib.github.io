9fa564033a261f8cb6d8044c77b44431fdf2292f
Renato Ribeiro <renatoribroman@gmail.com>
2019-03-16 15:34:16 -0300

    rm .cache public

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/home/renatorib/p/renato/node_modules/gatsby-mdx/gatsby-browser.js'),
-      options: {"plugins":[],"defaultLayouts":{"default":"/home/renatorib/p/renato/src/components/layout.js"}},
-    }]
