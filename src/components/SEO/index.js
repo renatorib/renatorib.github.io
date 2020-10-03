f1b8cdfdf5b65c9120c628c357a28b684de911be
Renato <renatoribroman@gmail.com>
2020-10-02 21:38:30 -0300

    fix absolute url in SEO

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -13,7 +13,7 @@ const SEO = ({ title, description, image, date }) => (
     {/* Open Graph */}
     {title && <meta property="og:title" content={title} />}
     {description && <meta property="og:description" content={description} />}
-    {image && <meta property="og:image" content={`${BASE_URL}/${image}`} />}
+    {image && <meta property="og:image" content={`${BASE_URL}${image}`} />}
 
     {/* Twiter Cards */}
     <meta name="twitter:card" content="summary_large_image" />
@@ -21,7 +21,7 @@ const SEO = ({ title, description, image, date }) => (
     <meta name="twitter:creator" content="@renatorib_" />
     {title && <meta name="twitter:title" content={title} />}
     {description && <meta name="twitter:description" content={description} />}
-    {image && <meta name="twitter:image" content={`${BASE_URL}/${image}`} />}
+    {image && <meta name="twitter:image" content={`${BASE_URL}${image}`} />}
 
     <link rel="shortcut icon" type="image/png" href={favicon} />
   </Head>
