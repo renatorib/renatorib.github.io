51e80fefbf1bffb1b9758faa1ff7f244391b5bf9
Renato <renatoribroman@gmail.com>
2020-10-03 21:52:20 -0300

    fix parser to accept whitespace as prop value

--- src/components/mdx/Paragraph.js
+++ src/components/mdx/Paragraph.js
@@ -11,17 +11,21 @@ const customComponents = {
 const Paragraph = props => {
   const { textColor, textFontFamily } = useTheme();
 
+  // This is a workaround to acess custom components using mdx
+  // with the --> start string
+  // e.g.: -->[Component prop1=value,prop2=value]
   if (
     typeof props.children[0] === "string" &&
     props.children[0].startsWith("-->")
   ) {
     const [, description, ...children] = props.children;
-    const [componentName, propsRaw = ""] = description
+    const [componentName, ...propsRaw] = description
       .replace(/\[/g, "")
       .replace(/\]/g, "")
       .split(" ");
 
     const propsToPass = propsRaw
+      .join(" ")
       .split(",")
       .map(propRaw => propRaw.split("="))
       .reduce((acc, prop) => ({ ...acc, [prop[0]]: prop[1] }), {});
